03a4972a98cf79ef961f4c3a01d4dc430819f6bf
Nathan Gemmill <nathan.gemmill@itgo.com.au>
2022-05-09 23:10:53 +1000

    Initial build of script (not working in Datto RMM due to env variable requiring no quotations)

---
+++ Check for KB Presence.ps1
@@ -0,0 +1,31 @@
+﻿#----------------------------------------------------------------
+#	Type:	Script
+#	Desc:	Fill UDF20 with specific KB presence
+#	Author:	Nathan Gemmill
+#	Ver:	1.0
+#----------------------------------------------------------------
+
+#----------------------------------------------------------------
+# Variables
+#----------------------------------------------------------------
+$KBString = "KB5007575","KB5012121"
+[array]$KBArray = @($KBString -Split(","))
+
+#----------------------------------------------------------------
+# Loop through KB array to find KB's and write presence to UDF20
+#----------------------------------------------------------------
+foreach ($KB in $KBArray)
+{
+  Write-Host Checking for $KB presence
+  Get-HotFix -Id $KB -ErrorAction Ignore
+  if (Get-HotFix | Where-Object {$_.HotfixID -eq $KB}) 
+  {
+  Write-Host $KB present
+  New-ItemProperty HKLM:\SOFTWARE\CentraStage -Name Custom20 -Value "$KB present"
+  break
+  }
+  else
+  {
+  New-ItemProperty HKLM:\SOFTWARE\CentraStage -Name Custom20 -Value "Required update is not present"
+  }
+}
